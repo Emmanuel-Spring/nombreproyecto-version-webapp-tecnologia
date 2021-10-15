669c1252947ebf4e86279ff58168e153a2ed5dfd
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-10-15 17:48:39 -0300

    Sistema de Facturación

---
+++ src/main/java/com/talentyco/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.talentyco.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Java Title
+ *
+ * @author Emmanuel Nieto Muñoz
+ * @version: xx/10/2021/1.0
+ * @see <a href = "" />  </a>
+ */
+
+@Controller
+public class HomeController {
+
+
+}
---
+++ src/main/java/com/talentyco/models/entity/Factura.java
@@ -0,0 +1,14 @@
+package com.talentyco.models.entity;
+
+/**
+ * Java Title
+ *
+ * @author Emmanuel Nieto Muñoz
+ * @version: xx/10/2021/1.0
+ * @see <a href = "" />  </a>
+ */
+
+public class Factura {
+
+
+}
